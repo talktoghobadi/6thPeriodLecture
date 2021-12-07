e80ceffd01f6e76677aa57e4bb1be9cd094c8bae
Maryam Ghobadi <talktoghobadi@gmail.com>
2021-12-07 00:59:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Get the first question from the User 
+question = input("Ask the mighty oracle a question, or enter q to quit ")
+
+#Repeat while the haven't entered q
+while question != "q":
+
+
+#Print the answer
+  print("The oracle is too hungry to answer. Come back later. ")
+
+
+#Update the question variable with a new question
+  question = input("Ask the oracle another question, or enter q to quit ")
+
+
+#Say goodbye once they have quit
+  print("Bye, bring food next time!")
